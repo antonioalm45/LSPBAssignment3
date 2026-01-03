4d239f2163fbaf2cd3ec2a038296f90a1936239e
Antonio Almenara <antonioalm45@gmail.com>
2026-01-03 17:37:28 +0100

    modify to send assignment 6 part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -10,13 +10,25 @@
 #include <sys/types.h>
 #include <syslog.h>
 #include <signal.h>
+#include <pthread.h>
+#include <time.h>
 
 #define PORT 9000
 #define BACKLOG 1
 #define DATAFILE "/var/tmp/aesdsocketdata"
 #define BUFFER_SIZE 1024
 
+// Estructura para pasar datos a los hilos
+typedef struct thread_data {
+    int client_fd;
+    struct sockaddr_in client_addr;
+    pthread_t thread_id;
+    bool completed;
+    struct thread_data *next;
+} thread_data_t;
+
 static volatile sig_atomic_t exit_requested = 0;
+pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void signal_handler(int sig)
 {
@@ -25,151 +37,224 @@ void signal_handler(int sig)
     syslog(LOG_INFO, "Caught signal, exiting");
 }
 
-int main(int argc, char *argv[]) {
-    bool daemon_mode = false;
+// HILO DEL TEMPORIZADOR (Escribe cada 10s)
+void *timer_thread(void *arg) {
+    (void)arg;
+    while (!exit_requested) {
+        // Esperar 10 segundos (usando sleep en intervalos cortos para responder rápido al exit)
+        for (int i = 0; i < 10 && !exit_requested; i++) {
+            sleep(1);
+        }
+        
+        if (exit_requested) break;
 
-    int server_fd = -1, client_fd = -1;
-    struct sockaddr_in server_address, client_address;
-    int opt = 1;
-    int client_len = sizeof(client_address);
-    char recv_buf[BUFFER_SIZE] = {0};
-    char send_buf[BUFFER_SIZE] = {0};
-    ssize_t bytes_received = 0;
-    FILE *fp = NULL;
+        time_t rawtime;
+        struct tm *info;
+        char timestamp[100];
 
-    // 1. Verificar si se pasó el argumento -d
-    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
-        daemon_mode = true;
+        time(&rawtime);
+        info = localtime(&rawtime);
+        // Formato RFC 2822: %a, %d %b %Y %H:%M:%S %z
+        strftime(timestamp, sizeof(timestamp), "timestamp:%a, %d %b %Y %H:%M:%S %z\n", info);
+
+        pthread_mutex_lock(&file_mutex);
+        FILE *fp = fopen(DATAFILE, "a+");
+        if (fp) {
+            fputs(timestamp, fp);
+            fclose(fp);
+        }
+        pthread_mutex_unlock(&file_mutex);
+    }
+    return NULL;
+}
+
+void *handle_connection(void *arg) {
+    thread_data_t *data = (thread_data_t *)arg;
+    char client_ip[INET_ADDRSTRLEN];
+    char *recv_buf = malloc(BUFFER_SIZE);
+    char *send_buf = malloc(BUFFER_SIZE);
+    
+    inet_ntop(AF_INET, &data->client_addr.sin_addr, client_ip, sizeof(client_ip));
+    
+    // El archivo debe abrirse y cerrarse dentro del hilo o manejarse con cuidado
+    // Usaremos fopen/fclose dentro de la sección crítica para asegurar consistencia
+    
+    ssize_t bytes_received;
+    bool newline_found = false;
+
+    while (!exit_requested && (bytes_received = recv(data->client_fd, recv_buf, BUFFER_SIZE, 0)) > 0) {
+        
+        pthread_mutex_lock(&file_mutex);
+        FILE *fp = fopen(DATAFILE, "a+");
+        if (!fp) {
+            syslog(LOG_ERR, "File open failed: %s", strerror(errno));
+            close(data->client_fd);
+            continue;
+        }
+        if (fp) {
+            // 1. Obtener el tiempo actual
+            time_t rawtime;
+            struct tm *info;
+            char timestamp[64];
+
+            time(&rawtime);
+            info = localtime(&rawtime);
+
+            // 2. Formatear la fecha según RFC 2822
+            // %a: nombre día semana abreviado, %d: día, %b: mes abreviado, %Y: año, %H:%M:%S: hora, %z: zona horaria
+            strftime(timestamp, sizeof(timestamp), "%a, %d %b %Y %H:%M:%S %z", info);
+
+            // 3. Escribir primero el timestamp (opcionalmente con un prefijo para identificarlo)
+            fprintf(fp, "timestamp:%s\n", timestamp);
+            fwrite(recv_buf, 1, bytes_received, fp);
+            fclose(fp);
+        }
+        pthread_mutex_unlock(&file_mutex);
+
+        if (memchr(recv_buf, '\n', bytes_received)) {
+            newline_found = true;
+            break;
+        }
     }
 
-    // 3. Entrar en modo demonio si es necesario
+    if (newline_found) {
+        pthread_mutex_lock(&file_mutex);
+        FILE *fp = fopen(DATAFILE, "r");
+        if (fp) {
+            size_t bytes_read;
+            while ((bytes_read = fread(send_buf, 1, BUFFER_SIZE, fp)) > 0) {
+                send(data->client_fd, send_buf, bytes_read, 0);
+            }
+            fclose(fp);
+        }
+        pthread_mutex_unlock(&file_mutex);
+    }
+
+    close(data->client_fd);
+    syslog(LOG_INFO, "Closed connection from %s", client_ip);
+    
+    free(recv_buf);
+    free(send_buf);
+    data->completed = true;
+    return arg;
+}
+
+int main(int argc, char *argv[]) {
+    bool daemon_mode = (argc > 1 && strcmp(argv[1], "-d") == 0);
+
     if (daemon_mode) {
         pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork");
-            exit(-1);
-        }
-        if (pid > 0) {
-            // Proceso padre termina
-            exit(0);
-        }
-        
-        // Proceso hijo: Crear nueva sesión y cambiar directorio
-        if (setsid() < 0) exit(-1);
-        if (chdir("/") < 0) exit(-1);
-        
-        // Opcional: Redirigir descriptores de archivo estándar a /dev/null
-        open("/dev/null", O_RDWR); // stdin
-        (void)dup(0); // stdout
-        (void)dup(0); // stderr
+        if (pid < 0) exit(-1);
+        if (pid > 0) exit(0);
+        setsid();
+        chdir("/");
+        int devnull = open("/dev/null", O_RDWR);
+        dup2(devnull, STDIN_FILENO);
+        dup2(devnull, STDOUT_FILENO);
+        dup2(devnull, STDERR_FILENO);
     }
 
-    /* Register signal handlers */
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = signal_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;   // IMPORTANTE: NO SA_RESTART
-
+    struct sigaction sa = {.sa_handler = signal_handler};
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
 
-    // Open syslog
-    openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER);
+    openlog("aesdsocket", LOG_PID, LOG_USER);
 
-    // Create socket file descriptor
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        syslog(LOG_ERR, "Socket creation failed: %s", strerror(errno));
-        return -1;
-    }
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    // Set socket options
-    // level=SOL_SOCKET, which indicates an option that applies at the sockets API level.
-    // optname=SO_REUSEADDR, which allows the socket to bind to an address that is already in use.
-    opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        syslog(LOG_ERR, "Set socket options failed: %s", strerror(errno));
-        close(server_fd);
-        return -1;
-    }
-
-    // Bind the socket
-    // Initialize address structure
-    // sin_family: Address family (AF_INET for IPv4)
-    // sin_addr.s_addr: IP address (INADDR_ANY to bind to all interfaces)
-    // sin_port: Port number (converted to network byte order using htons)
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = htons(PORT);
+    struct sockaddr_in server_address = {
+        .sin_family = AF_INET,
+        .sin_addr.s_addr = htonl(INADDR_ANY),
+        .sin_port = htons(PORT)
+    };
 
     if (bind(server_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
         syslog(LOG_ERR, "Bind failed: %s", strerror(errno));
         close(server_fd);
         return -1;
     }
-
-    // Listen for incoming connections
     if (listen(server_fd, BACKLOG) < 0) {
         syslog(LOG_ERR, "Listen failed: %s", strerror(errno));
         close(server_fd);
         return -1;
     }
 
+    // LANZAR EL HILO DEL TEMPORIZADOR
+    pthread_t timer_tid;
+    pthread_create(&timer_tid, NULL, timer_thread, NULL);
+
+    thread_data_t *head = NULL;
+
+    
+
     while (!exit_requested) {
-        // Accept a connection
-        // client_fd: New socket file descriptor for the accepted connection
-        // client_address: Pointer to sockaddr structure to hold client client_address
-        // client_len: Pointer to socklen_t variable that holds the size of the client_address structure
-        if ((client_fd = accept(server_fd, (struct sockaddr *)&client_address,
-                                 (socklen_t*)&client_len)) < 0) {
+        struct sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+
+        if (client_fd < 0) {
             if (errno == EINTR && exit_requested) 
                 break;
             syslog(LOG_ERR, "Accept failed: %s", strerror(errno));
             close(server_fd);
+            
             continue;
         }
-        // Logs message to the syslog “Accepted connection from xxx” where XXXX is the IP client_address of the connected client. 
+
         char client_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &client_address.sin_addr, client_ip, sizeof(client_ip));
+        inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
 
-        // Open data file
-        // a+ mode: Open for reading and appending; create file if it doesn't exist
-        fp = fopen(DATAFILE, "a+");
-        if (!fp) {
-            syslog(LOG_ERR, "File open failed: %s", strerror(errno));
-            close(client_fd);
-            close(server_fd);
-            continue;
-        }
+        // Crear nodo para el nuevo hilo
+        thread_data_t *new_thread = malloc(sizeof(thread_data_t));
+        new_thread->client_fd = client_fd;
+        new_thread->client_addr = client_addr;
+        new_thread->completed = false;
+        new_thread->next = head;
+        head = new_thread;
 
-        // Read data from socket and write to file
-        // bytes_received: Number of bytes read from socket
-        while (!exit_requested && (bytes_received = read(client_fd, recv_buf, BUFFER_SIZE)) > 0) {
-            fwrite(recv_buf, 1, bytes_received, fp);
-            fflush(fp);
-            if (memchr(recv_buf, '\n', bytes_received)) {
-                rewind(fp);
+        pthread_create(&new_thread->thread_id, NULL, handle_connection, new_thread);
 
-                while ((bytes_received = fread(send_buf, 1, BUFFER_SIZE, fp)) > 0) {
-                    send(client_fd, send_buf, bytes_received, 0);
-                }
-                break;
+        // Limpieza de hilos finalizados (Join)
+        thread_data_t *curr = head, *prev = NULL;
+        while (curr) {
+            if (curr->completed) {
+                pthread_join(curr->thread_id, NULL);
+                if (prev) prev->next = curr->next;
+                else head = curr->next;
+                
+                thread_data_t *temp = curr;
+                curr = curr->next;
+                free(temp);
+            } else {
+                prev = curr;
+                curr = curr->next;
             }
         }
-
-        fclose(fp);
-        close(client_fd);
-        syslog(LOG_INFO, "Closed connection from %s", client_ip);
-         
-    }  
+    }
 
     /* Cleanup */
+    while (head) {
+        pthread_join(head->thread_id, NULL);
+        thread_data_t *temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    // Limpieza final
+    pthread_join(timer_tid, NULL);
+    while (head) {
+        pthread_join(head->thread_id, NULL);
+        thread_data_t *temp = head;
+        head = head->next;
+        free(temp);
+    }
+
     close(server_fd);
     remove(DATAFILE);
+    pthread_mutex_destroy(&file_mutex);
     closelog();
-
     return 0;
 }
